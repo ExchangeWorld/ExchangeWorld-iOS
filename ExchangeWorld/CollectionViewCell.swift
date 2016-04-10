826835b9af655d167ba2adf0d53295dfc4e7606d
a36980097 <a36980097@gmail.com>
2016-04-10 17:50:50 +0800

    瀑布流upload (改學長)

---
+++ ExchangeWorld/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  ExchangeWorld
+//
+//  Created by 王志尹 on 2016/4/10.
+//  Copyright © 2016年 Exwd. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
