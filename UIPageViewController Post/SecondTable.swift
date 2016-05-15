d91a412297f821ce26fc2124d0a6ddc13f36e7fe
a36980097 <a36980097@gmail.com>
2016-05-15 22:15:26 +0800

    adding losing file

---
+++ UIPageViewController Post/SecondTable.swift
@@ -0,0 +1,14 @@
+//
+//  SecondTable.swift
+//  UIPageViewController Post
+//
+//  Created by 王志尹 on 2016/5/15.
+//  Copyright © 2016年 Seven Even. All rights reserved.
+//
+
+import Foundation
+
+struct SecondTable{
+    var ObjectImage : [String]
+}
+
