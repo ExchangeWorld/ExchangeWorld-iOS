ba1836e7a3040c3afdb02da1a31f6ca8c61a266c
a36980097 <a36980097@gmail.com>
2016-12-03 21:34:32 +0800

    post done

--- ExchangeWorld/uploadaViewController.swift
+++ ExchangeWorld/uploadaViewController.swift
@@ -8,56 +8,92 @@
 
 import UIKit
 
-struct cellData {
-    let cell : Int!
-    let text : String!
-}
 
-class uploadViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate,UITableViewDelegate, UITableViewDataSource {
+class uploadViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate,UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate {
     
     @IBOutlet weak var cameraButton: UIButton!
     @IBOutlet weak var photoLibraryButton: UIButton!
     @IBOutlet weak var imageDisplay: UIImageView!
+    
+    
     @IBOutlet weak var userInputTitleLabel: UILabel!
-    @IBOutlet weak var userInputTableView: UITableView!
+    @IBOutlet weak var userInputView: UIView!
+    @IBOutlet weak var uploadNameTitleLabel: UILabel!
+    @IBOutlet weak var uploadNameTextField: UITextField!
+    @IBOutlet weak var uploadCategoryTitleLabel: UILabel!
+    @IBOutlet weak var uploadCategoryTextField: UITextField!
+    @IBOutlet weak var uploadContextTitleLabel: UILabel!
+    @IBOutlet weak var uploadContextTextView: UITextView!
+    
     var pickedImage: UIImage!
-    var arrayOfCellData = [cellData]()
+    
+    
+    let categories = ["書籍", "教科書", "電子產品", "3C配件", "服飾", "化妝品", "飾品", "食物", "家庭用品", "體育用品", "電玩遊戲", "其他"]
+    let picker = UIPickerView()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let picker = UIImagePickerController()
         let screenSize: CGRect = UIScreen.main.bounds
         
-        picker.delegate = self
-        picker.sourceType = .photoLibrary
-        
-        present(picker, animated: true, completion: nil)
         self.view.backgroundColor = UIColor(red: 245.0/255.0, green: 245.0/255.0, blue: 245.0/255.0, alpha: 1.0)
-        userInputTitleLabel.frame = CGRect(x: screenSize.width*0.02, y: screenSize.height*0.44, width: screenSize.width*0.3, height: screenSize.width*0.05)
         userInputTitleLabel.font = UIFont.systemFont(ofSize: 17)
         userInputTitleLabel.textColor = UIColor(red: 100.0/255.0, green: 100.0/255.0, blue: 100.0/255.0, alpha: 1.0)
-        userInputTableView.frame = CGRect(x: 10, y: screenSize.height*0.48, width: screenSize.width-20, height: 279)
+        let heightConstraint = NSLayoutConstraint(item: userInputView, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1, constant: screenSize.height*0.35)
+        userInputView.addConstraint(heightConstraint)
         
-        arrayOfCellData = [cellData(cell: 1, text: "商品名稱（必填）"),
-                           cellData(cell: 2, text: "商品種類"),
-                           cellData(cell: 3, text: "商品敘述")]
+        uploadNameTitleLabel.textColor = UIColor(red: 210.0/255.0, green: 210.0/255.0, blue: 210.0/255.0, alpha: 1.0)
+        uploadNameTitleLabel.text = "商品名稱"
+        uploadNameTextField.backgroundColor = UIColor(red: 240.0/255.0, green: 240.0/255.0, blue: 240.0/255.0, alpha: 1.0)
+        uploadNameTextField.textColor = UIColor.black
+        uploadCategoryTitleLabel.text = "商品種類"
+        uploadCategoryTitleLabel.textColor = uploadNameTitleLabel.textColor
+        uploadCategoryTextField.backgroundColor = uploadNameTextField.backgroundColor
+        uploadCategoryTextField.textColor = UIColor.black
+        uploadContextTitleLabel.text = "商品描述"
+        uploadContextTitleLabel.textColor = uploadNameTitleLabel.textColor
+        uploadContextTextView.backgroundColor = uploadNameTextField.backgroundColor
+        uploadContextTextView.textColor = UIColor.black
+        
+
+        picker.delegate = self
+        picker.dataSource = self
+        uploadCategoryTextField.inputView = picker
         
     }
     
+
+    
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return categories.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        uploadCategoryTextField.text = categories[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return categories[row]
+    }
+    
+    
+    
     @IBAction func postingObjectButton(_ sender: Any) {
         print("111")
-        let cell1 = Bundle.main.loadNibNamed("uploadNameTableViewCell", owner: self, options: nil)?.first as!uploadNameTableViewCell
-            print(cell1.uploadNameTitleLabel.text)
-            print(cell1.uploadNameTextField.text)
-            let cell2 = Bundle.main.loadNibNamed("uploadCategoryTableViewCell", owner: self, options: nil)?.first as! uploadCategoryTableViewCell
-            print(cell2.uploadCategoryTitleLabel.text)
-            print(cell2.uploadCategoryTextField.text)
-
-        //print(uploadNameTableViewCell().uploadNameTitleLabel.text)
-        //print(uploadNameTableViewCell().uploadNameTitleLabel)
+        httpPost(URL: "\(Constants.API_SERVER_URL)/api/upload/image?token=\(Constants.exwdToken)", parameters: ["filesize":Constants.imageSize, "filename": "\(uploadNameTextField.text)" , "base64": "\(Constants.imageInBase64)", "filetype": Constants.imageType],returnJsonFormat: false)
+        
+        httpPost(URL: "\(Constants.API_SERVER_URL)/api/goods/post?token=\(Constants.exwdToken)", parameters: ["name":"\(uploadNameTextField.text!)", "category":"\(uploadCategoryTextField.text!)", "description":"\(uploadContextTextView.text!)", "photo_path":"[\"\(Constants.imageURL)\"]", "position_x":121.5453914, "position_y":25.0261973],returnJsonFormat: true)
+        
     }
+    
+    
     @IBAction func cameraAction(_ sender: Any) {
         let picker = UIImagePickerController()
         
@@ -90,90 +126,6 @@ class uploadViewController: UIViewController, UIImagePickerControllerDelegate, U
             print(Constants.imageType)
         }
         
-//        let imageData: NSData = UIImagePNGRepresentation(self.pickedImage) as NSData!
-//        let imageBase64: String = imageData.base64EncodedData(options: .lineLength64Characters)
-    }
-    
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        guard let segueIdentifier = segue.identifier else {
-//            return
-//        }
-//        
-//        if segueIdentifier == "sendImage" {
-//            print("111111111")
-//            let cell1 = Bundle.main.loadNibNamed("uploadNameTableViewCell", owner: self, options: nil)?.first as! uploadNameTableViewCell
-//            print(cell1.uploadNameTextField.text!)
-//            let cell2 = Bundle.main.loadNibNamed("uploadCategoryTableViewCell", owner: self, options: nil)?.first as! uploadCategoryTableViewCell
-//            print(cell2.uploadCategoryTextField.text!)
-//            
-//            
-//            
-//            //httpPost(URL: "\(Constants.API_SERVER_URL)/api/upload/image?token=\(Constants.exwdToken)", parameters: ["filesize":Constants.imageSize, "filename": "uploadImgTRY1" , "base64": "\(Constants.imageInBase64)", "filetype": Constants.imageType],returnJsonFormat: false)
-//            
-//            //httpPost(URL: "\(Constants.API_SERVER_URL)/api/goods/post?token=\(Constants.exwdToken)", parameters: ["name":"1203try1", "category":"Others", "description":"1203try1", "photo_path":"[\"\(Constants.imageURL)\"]", "position_x":121.5453914, "position_y":25.0261973],returnJsonFormat: true)
-//            
-////            guard let detailViewController = segue.destination as? HomeObjectCollectionViewController else {
-////                return
-////            }
-////            guard let cell = sender as? UICollectionViewCell else {
-////                return
-////            }
-////            let indexPath = self.collectionView?.indexPath(for: cell)
-////            
-////            detailViewController.categoryName = self.categoryImageArray[indexPath!.row]
-////        }
-//        }
-//    
-//    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return arrayOfCellData.count
-    }
-    
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if arrayOfCellData[indexPath.row].cell == 1{
-            
-            let cell = Bundle.main.loadNibNamed("uploadNameTableViewCell", owner: self, options: nil)?.first as! uploadNameTableViewCell
-            cell.uploadNameTitleLabel.text = arrayOfCellData[indexPath.row].text
-            
-            return cell
-        }
-        else if arrayOfCellData[indexPath.row].cell == 2{
-            let cell = Bundle.main.loadNibNamed("uploadCategoryTableViewCell", owner: self, options: nil)?.first as! uploadCategoryTableViewCell
-            cell.uploadCategoryTitleLabel.text = arrayOfCellData[indexPath.row].text
-            
-            return cell
-        }
-        else if arrayOfCellData[indexPath.row].cell == 3{
-            let cell = Bundle.main.loadNibNamed("uploadContextTableViewCell", owner: self, options: nil)?.first as! uploadContextTableViewCell
-            cell.uploadContextTitleLabel.text = arrayOfCellData[indexPath.row].text
-            return cell
-        }
-        else{
-            
-            let cell = Bundle.main.loadNibNamed("uploadNameTableViewCell", owner: self, options: nil)?.first as! uploadNameTableViewCell
-            cell.uploadNameTitleLabel.text = arrayOfCellData[indexPath.row].text
-            
-            return cell
-        }
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        if arrayOfCellData[indexPath.row].cell == 1{
-            
-            return 80
-        }
-        else if arrayOfCellData[indexPath.row].cell == 2{
-            return 80
-        }
-        else if arrayOfCellData[indexPath.row].cell == 3{
-            return 120
-        }
-        else{
-            return 80
-            
-        }
     }
     
         
